d250d631b3563f29ff46ee4620fb1490c87af351
agapealex <agapee.alex@yahoo.com>
2023-03-24 18:41:13 +0200

    add gitignore

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+app.get('/home', function(req, res) {
+  res.send('hoooome');
+});
+app.listen(5000, function() {
+  console.log('Serverul ruleaza pe portul 5000!');
+});
