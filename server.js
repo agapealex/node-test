7e2641a22646d3b3f514a498df18ef44f055f574
agapealex <agapee.alex@yahoo.com>
2023-03-24 18:49:01 +0200

    fhf

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
+
 app.get('/home', function(req, res) {
   res.send('hoooome');
 });
+
 app.listen(5000, function() {
   console.log('Serverul ruleaza pe portul 5000!');
 });  
