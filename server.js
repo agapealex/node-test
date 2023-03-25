7b5151e29003bae70210dfccf6387d9022234c83
agapealex <agapee.alex@yahoo.com>
2023-03-25 10:07:24 +0200

    sgfdg

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
+
 const app = express();
+
+
+app.use(cors()); 
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
